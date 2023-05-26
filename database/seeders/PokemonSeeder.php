1810b2c863d05add3cf36bb29e5700c7e82748ca
RobertoNesta <robertonesta1993@gmail.com>
2023-05-26 16:42:05 +0200

    faker added

--- database/seeders/PokemonSeeder.php
+++ database/seeders/PokemonSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Pokemon;
 
 class PokemonSeeder extends Seeder
 {
@@ -11,8 +13,22 @@ class PokemonSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 20; $i++){
+            $pokemon = new Pokemon();
+            $pokemon->name = $faker->words(1, true);
+            $pokemon->image = $faker->words(3, true);
+            $pokemon->primary_type = $faker->words(1, true);
+            $pokemon->secondary_type = $faker->words(1, true);
+            $pokemon->order = $faker->numberBetween(1, 150);
+            $pokemon->height = $faker->words(1, true);
+            $pokemon->weight = $faker->words(1, true);
+            $pokemon->species = $faker->randomElement(['normal', 'fire', 'water', 'grass', 'electric', 'ice','poison', 'psico', 'flying', 'dragon']);
+            $pokemon->caught = $faker->randomElement([1, 0]);
+            $pokemon->gender = $faker->randomElement(['M', 'F', 'U']);
+            $pokemon->save();
+
+        }
     }
 }
