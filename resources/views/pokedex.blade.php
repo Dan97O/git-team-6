d984fc2df487afbab4d287741c3d56068b7c80b8
Dan97O <dan97-oddo@live.it>
2023-06-01 17:24:10 +0200

    edit card-body

--- resources/views/pokedex.blade.php
+++ resources/views/pokedex.blade.php
@@ -1,32 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-<section>
-    <div class="container">
-        <div class="row">
-            @foreach($pokemons as $pokemon)
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h2>Name: {{$pokemon->name}}</h2>
+    <section>
+        <div class="container">
+            <div class="row">
+                @foreach ($pokemons as $pokemon)
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h2>Name: {{ $pokemon->name }}</h2>
+                            </div>
+                            <div class="card-body">
+                                <h3>Type 1: {{ $pokemon->type_1 }}</h3>
+                                <h3>Type 2: {{ $pokemon->type_2 }}</h3>
+                                <h3>Total: {{ $pokemon->total }}</h3>
+                                <h3>HP: {{ $pokemon->hp }}</h3>
+                                <h3>Attack: {{ $pokemon->attack }}</h3>
+                                <h3>Defense: {{ $pokemon->defense }}</h3>
+                                <h3>Special Attack: {{ $pokemon->sp_atk }}</h3>
+                                <h3>Special Defense: {{ $pokemon->sp_def }}</h3>
+                                <h3>Speed: {{ $pokemon->speed }}</h3>
+                                <h3>Generation: {{ $pokemon->generation }}</h3>
+                                <h3>Legendary: {{ $pokemon->legendary ? 'Yes' : 'No' }}</h3>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <h2>Type: {{$pokemon->type_1}}</h2>
-                        <h2>Type: {{$pokemon->type_2}}</h2>
-                        <h2>Type: {{$pokemon->total}}</h2>
-                        <h2>Type: {{$pokemon->hp}}</h2>
-                        <h2>Type: {{$pokemon->attack}}</h2>
-                        <h2>Type: {{$pokemon->defense}}</h2>
-                        <h2>Type: {{$pokemon->sp_atk}}</h2>
-                        <h2>Type: {{$pokemon->sp_def}}</h2>
-                        <h2>Type: {{$pokemon->speed}}</h2>
-                        <h2>Type: {{$pokemon->generation}}</h2>
-                        <h2>Type: {{$pokemon->legendary}}</h2>
-                    </div>
-                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
-    </div>
-</section>
+    </section>
 @endsection
