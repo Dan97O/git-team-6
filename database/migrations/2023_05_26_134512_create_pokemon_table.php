a1621258e12a5d964273ab85d2091b04277153e4
Dan97O <dan97-oddo@live.it>
2023-06-02 17:47:44 +0200

    add csv with pokemon image, and stylized the cards

--- database/migrations/2023_05_26_134512_create_pokemon_table.php
+++ database/migrations/2023_05_26_134512_create_pokemon_table.php
@@ -13,20 +13,36 @@ return new class extends Migration
      */
     public function up()
     {
+        /*    Schema::create('pokemon', function (Blueprint $table) {
+        $table->id();
+        $table->string('name', 100);
+        $table->string('type_1', 30);
+        $table->string('type_2', 30)->nullable();
+        $table->integer('total');
+        $table->integer('hp');
+        $table->integer('attack');
+        $table->integer('defense');
+        $table->integer('sp_atk');
+        $table->integer('sp_def');
+        $table->integer('speed');
+        $table->integer('generation');
+        $table->boolean('legendary')->default(false);
+        $table->timestamps();
+        }); */
+
         Schema::create('pokemon', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
-            $table->string('type_1', 30);
-            $table->string('type_2', 30)->nullable();
-            $table->integer('total');
-            $table->integer('hp');
-            $table->integer('attack');
-            $table->integer('defense');
-            $table->integer('sp_atk');
-            $table->integer('sp_def');
-            $table->integer('speed');
-            $table->integer('generation');
-            $table->boolean('legendary')->default(false);
+            $table->string('name', 70);
+            $table->string('type_1', 50);
+            $table->string('type_2', 50)->nullable();
+            $table->integer('hp')->nullable();
+            $table->integer('attack')->nullable();
+            $table->integer('defense')->nullable();
+            $table->integer('speed')->nullable();
+            $table->integer('special')->nullable();
+            $table->text('gif')->nullable();
+            $table->text('png')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/PokemonSeeder.php
+++ database/seeders/PokemonSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use App\Utilities\Helpers;
 use App\Models\Pokemon;
+use App\Utilities\Helpers;
+use Illuminate\Database\Seeder;
 
 class PokemonSeeder extends Seeder
 {
@@ -16,28 +15,48 @@ class PokemonSeeder extends Seeder
      */
     public function run()
     {
-        $pokemons = Helpers::getCsvData(__DIR__ . "/pokemon.csv");
-        foreach($pokemons as $pokemon => $row) {
-            if($pokemon != 0) {
+        /*     $pokemons = Helpers::getCsvData(__DIR__ . "/pokemon.csv");
+        foreach ($pokemons as $pokemon => $row) {
+        if ($pokemon != 0) {
+        $pokemon = new Pokemon();
+        $pokemon->name = $row[1];
+        $pokemon->type_1 = $row[2];
+        $pokemon->type_2 = $row[3];
+        $pokemon->total = $row[4];
+        $pokemon->hp = $row[5];
+        $pokemon->attack = $row[6];
+        $pokemon->defense = $row[7];
+        $pokemon->sp_atk = $row[8];
+        $pokemon->sp_def = $row[9];
+        $pokemon->speed = $row[10];
+        $pokemon->generation = $row[11];
+        if ($row[12] === "True") {
+        $pokemon->legendary = true;
+        } elseif ($row[12] === "False") {
+        $pokemon->legendary = false;
+        }
+        $pokemon->save();
+        }
+        } */
+
+        $pokemons = Helpers::getCsvData(__DIR__ . "/Pokemon-description-image.csv");
+        foreach ($pokemons as $pokemon => $row) {
+            if ($pokemon != 0) {
                 $pokemon = new Pokemon();
                 $pokemon->name = $row[1];
                 $pokemon->type_1 = $row[2];
                 $pokemon->type_2 = $row[3];
-                $pokemon->total = $row[4];
-                $pokemon->hp = $row[5];
-                $pokemon->attack = $row[6];
-                $pokemon->defense = $row[7];
-                $pokemon->sp_atk = $row[8];
-                $pokemon->sp_def = $row[9];
-                $pokemon->speed = $row[10];
-                $pokemon->generation = $row[11];
-                if ($row[12] === "True") {
-                    $pokemon->legendary = true;
-                } elseif ($row[12] === "False") {
-                    $pokemon->legendary = false;
-                }
+                $pokemon->hp = $row[4];
+                $pokemon->attack = $row[5];
+                $pokemon->defense = $row[6];
+                $pokemon->speed = $row[7];
+                $pokemon->special = $row[8];
+                $pokemon->gif = $row[9];
+                $pokemon->png = $row[10];
+                $pokemon->description = $row[11];
                 $pokemon->save();
             }
         }
+
     }
 }
